9e23215cded9267821d2669a0fd03359ece50826
human6sa <1415750220@qq.com>
2024-08-27 23:29:47 +0800

    Site updated: 2024-08-27 23:29:46

--- js/rdmbkg.js
+++ js/rdmbkg.js
@@ -1,14 +1,14 @@
 
 //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var bannerimg =[
-  "url(/img/Bing/AnnularEclipse_ZH-CN2345201060_1920x1080.webp)",
-  "url(/img/Bing/BubbleNebula_ZH-CN2787112807_1920x1080.webp)",
-  "url(/img/Bing/ChateauBeynac_ZH-CN8777586167_1920x1080.webp)",
-  "url(/img/Bing/Calakmul_ZH-CN5969688954_1920x1080.webp)",
-  "url(/img/Bing/CuscoCathedral_ZH-CN9834821723_1920x1080.webp)",
-  "url(/img/Bing/HallwylfjelletSunset_ZH-CN9300910376_1920x1080.webp)",
-  "url(/img/Bing/GCVenice_ZH-CN0993336331_1920x1080.webp)",
-  "url(/img/Bing/GoldenGinkgo_ZH-CN8507013452_1920x1080.webp)"
+  "url(/img/Bing/AnnularEclipse.webp)",
+  "url(/img/Bing/BubbleNebula.webp)",
+  "url(/img/Bing/ChateauBeynac.webp)",
+  "url(/img/Bing/Calakmul.webp)",
+  "url(/img/Bing/CuscoCathedral.webp)",
+  "url(/img/Bing/HallwylfjelletSunset.webp)",
+  "url(/img/Bing/GCVenice.webp)",
+  "url(/img/Bing/GoldenGinkgo.webp)"
 ];
 //获取banner图片总数，生成随机数
 var bannerindex =Math.ceil(Math.random() * (bannerimg.length)-1);
